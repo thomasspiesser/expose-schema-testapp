146d7f45882a8ed8b6919d92e99d8ea139716592
adrianhitulescu <adrian@hitulescu.com>
2015-10-10 20:47:09 +0200

    Added package code

--- packages/expose-schema/expose-schema.js
+++ packages/expose-schema/expose-schema.js
@@ -1,112 +1,104 @@
 AaasSchemas = new Mongo.Collection('aaas_schemas');
 
+Exposee = {
+    'detectCollections': function () {
+        // Note: this returns the actual mongo collection name
+        var collections = _.map(Mongo.Collection.getAll(), function (collection) {
+            return collection.instance;
+        });
+        return _.without(collections, 'meteor_autoupdate_clientVersions');
+    },
+    'detectSimpleSchemas': function () {
+        var schemas = [];
+        for (var k in window) {
+            if (window[k] instanceof SimpleSchema) {
+                schemas.push(k);
+            }
+        }
+        return schemas;
+    },
+};
+
 // Write your package code here!
 Meteor.startup(function () {
-
-    bookingSchema = new SimpleSchema(bookingObject);
-
-    Bookings.attachSchema(bookingSchema);
-
-    var schema = Bookings._c2._simpleSchema._schema;
-
-
-
-    var get_type = function (val, key) {
-        switch (val.type.name) {
-            case 'Number':
-            case 'String':
-            case 'Boolean':
-            case 'Date':
-                return this[val.type.name];
-
-            case 'Array':
-                return Array;
-
-            case 'Object':
-                if (typeof relations[key] == 'undefined') {
-                    return Object;
-                }
-                else {
-                    return;
-                }
-        }
+    if (! Meteor.isServer) {
+        return ;
     }
 
-    mymain = {};
-    relations = {};
+    AaasSchemas.remove({});
 
-    _.map(schema, function (val, key) {
-        if (/\./.test(key)) {
-            var parts = key.split('.');
-            var related_key = parts[0];
-            var sub_key = parts[1];
+    var collections = Exposee.detectCollections();
 
-            if (typeof relations[related_key] == 'undefined') {
-                relations[related_key] = {};
+    _.each(collections, function(Collection) {
+        var simpleSchema = Collection.simpleSchema();
+
+        if (! simpleSchema) {
+            return;
+        }
+
+        var get_type = function (val, key) {
+            switch (val.type.name) {
+                case 'Number':
+                case 'String':
+                case 'Boolean':
+                case 'Date':
+                    return this[val.type.name];
+
+                case 'Array':
+                    return Array;
+
+                case 'Object':
+                    if (typeof relations[key] == 'undefined') {
+                        return Object;
+                    }
+                    else {
+                        return;
+                    }
             }
-
-            relations[related_key][sub_key] = val;
-            relations[related_key][sub_key].type = get_type(val, key);
         }
-        else {
-            mymain[key] = val;
-            mymain[key].type = get_type(val, key);
 
-            /*
-             if (typeof mymain[key]['autoValue'] != 'undefined') {
-             if (typeof mymain[key].functions == 'undefined') mymain[key].functions = {};
+        var mymain = {};
+        var relations = {};
 
-             mymain[key].type = 'autovalue';
-             mymain[key].functions['autovalue'] = mymain[key]['autoValue'].toString();
-             }
-             */
-        }
+        _.map(simpleSchema._schema, function (val, key) {
+            if (/\./.test(key)) {
+                var parts = key.split('.');
+                var related_key = parts[0];
+                var sub_key = parts[1];
+
+                if (typeof relations[related_key] == 'undefined') {
+                    relations[related_key] = {};
+                }
+
+                if (sub_key == '$') {
+                    sub_key = '_dollar';
+                }
+
+                relations[related_key][sub_key] = val;
+                relations[related_key][sub_key].keyType = val.type.name;
+            }
+            else {
+                mymain[key] = val;
+                mymain[key].keyType = val.type.name;
+
+                /*
+                 if (typeof mymain[key]['autoValue'] != 'undefined') {
+                 if (typeof mymain[key].functions == 'undefined') mymain[key].functions = {};
+
+                 mymain[key].type = 'autovalue';
+                 mymain[key].functions['autovalue'] = mymain[key]['autoValue'].toString();
+                 }
+                 */
+            }
+        })
+
+        var doc = {
+            name: Collection._name,
+            main: mymain,
+            relations: relations
+        };
+
+        AaasSchemas.insert(doc);
     })
-
-    var add_schema = function (val, key) {
-        switch (mymain[key].type.name) {
-            case 'String':
-                break;
-
-            case 'Array':
-                mymain[key].type = [this[val.$.type.name]];
-                break;
-
-            case 'Object':
-                mymain[key].type = new SimpleSchema(val);
-                break;
-        }
-    }
-
-    _.map(relations, function (val, key) {
-        add_schema(val, key);
-    })
-
-    console.log(mymain);
-
-    _.map(mymain, function (val, key) {
-        console.log(key);
-        console.log(val.type);
-    })
-
-    NewSchema = new SimpleSchema(mymain);
-
-//Schemas.insert(newschema);
-
-//
-//var extr_schema = Schemas.findOne('5Fd6nsevPXZxdbZo9');
-//
-//var fixedschema = {};
-//_.map(extr_schema, function(val, key) {
-//  fixedschema[key.replace('_', '.')] = val;
-//})
-//
-//delete(fixedschema['.id']);
-//ExtractedBookings = new Mongo.Collection('extractedBookings');
-//ExtractedBookings.attachSchema(new SimpleSchema({}));
-//ExtractedBookings._c2._simpleSchema._schema = fixedschema;
-//
-//
-//
 
 });
