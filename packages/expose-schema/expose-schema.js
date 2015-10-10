4a0aa1b8d4c340a782f8e7117f8a3e68a02721e0
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-10-10 14:37:16 +0200

    adds collection2 package

--- packages/expose-schema/expose-schema.js
+++ packages/expose-schema/expose-schema.js
@@ -1,1 +1,5 @@
 // Write your package code here!
+Meteor.startup( function () {
+  console.log( 'here');
+  console.log( bookingSchema );
+} );
--- packages/expose-schema/package.js
+++ packages/expose-schema/package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'tomsp:expose-schema',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'stores the schema into a separate collection',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
